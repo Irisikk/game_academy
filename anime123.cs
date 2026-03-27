f4ef292f8ca7f2e654780cdb8ed1c65e54ed416d
mtcooller <mmtbro7@gmail.com>
2026-03-27 22:54:06 +0500

    TEST ANIMATION BLYAT`

---
+++ anime123.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class anime123 : AnimatedSprite2D
+{   
+    [Export]
+    public float Speed = 400.0f;
+
+    public override void _Process(double delta)
+    {
+        Vector2 velocity = Vector2.Zero;
+
+    
+        if (Input.IsActionPressed("moverightTEST")) velocity.X += 1;
+        if (Input.IsActionPressed("moveleftTEST"))  velocity.X -= 1;
+        if (Input.IsActionPressed("movedownTEST"))  velocity.Y += 1;
+        if (Input.IsActionPressed("moveupTEST"))    velocity.Y -= 1;
+
+        
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+        }
+
+    
+        Position += velocity * (float)delta;
+    }
+}
