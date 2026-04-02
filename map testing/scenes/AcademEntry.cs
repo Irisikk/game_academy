0e924d929a3db45d95bea7aefe2693e846652bfb
Irisikk <georgy.vidyakin@yandex.ru>
2026-04-02 22:38:25 +0300

    tests tests and one more time tests

--- map testing/scenes/AcademEntry.cs
+++ map testing/scenes/AcademEntry.cs
@@ -3,8 +3,29 @@ using System;
 
 public partial class AcademEntry : Area2D
 {
+    private bool ent = false;
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("interact") && ent) Enter(); 
+    }
+    
+    private void Enter()
+    {
+        GD.Print("HUIBLYAT");    
+        ProcessMode = ProcessModeEnum.Disabled;
+    }
+
     private void OnAreaEntered(Area2D area)
     {
+        ent = true;
+        GD.Print(area);
+        GD.Print("HHHHHHHH");
+    }
+
+    private void OnAreaExited(Area2D area)
+    {
+        ent = false;
         GD.Print(area);
     }
 }
