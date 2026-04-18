04bbc039f9d2691909d64571e7edd0a268b91d2f
Irisikk <georgy.vidyakin@yandex.ru>
2026-04-18 20:58:04 +0300

    finally changing scenes

---
+++ map testing/AcademEntry.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class AcademEntry : Area2D
+{
+    [Export]
+    public SceneTree retx;
+    public Node scene = ResourceLoader.Load<PackedScene>("res://map testing/scenes/intern_academy.tscn").Instantiate();
+
+    private bool ent = false;
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("interact") && ent) Enter(); 
+    }
+    
+    private void Enter()
+    {
+        GD.Print("HUIBLYAT");
+        // GetTree().UnloadCurrentScene();
+        // GD.Print("123123123123 IM EXIST"); // this exist after unload scene ¯\_(ツ)_/¯ 
+        // AddChild(scene);
+        GetTree().ChangeSceneToFile("res://map testing/scenes/intern_academy.tscn");
+        // ProcessMode = ProcessModeEnum.Disabled;
+        // GetTree().Paused = true; // this idk
+    }
+
+    private void OnAreaEntered(Area2D area)
+    {
+        ent = true;
+        GD.Print(area);
+        GD.Print("HHHHHHHH");
+    }
+
+    private void OnAreaExited(Area2D area)
+    {
+        ent = false;
+        GD.Print(area);
+    }
+}
